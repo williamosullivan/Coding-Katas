f89a3a77ad8f0d0f3605e9dd30bd4657f6c08838
William O'Sullivan <williamosullivan@gmail.com>
2014-09-05 07:24:00 -0400

    FizzBuzz
    Fizz Buzz solved

--- Coding-Katas/Program.cs
+++ Coding-Katas/Program.cs
@@ -10,6 +10,31 @@ namespace Coding_Katas
     {
         static void Main(string[] args)
         {
+            FizzBuzz();
+        }
+
+        private static void FizzBuzz()
+        {
+            int max = 100;
+            for (int i = 1; i <= max; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                        Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
